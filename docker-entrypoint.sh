d0048183cafc537e2931980c94b0fc3c8568a952
Christian Scheurer <git@tinytux.ch>
2018-05-30 22:25:39 +0200

    Removing qt examples
    Signed-off-by: Christian Scheurer <git@tinytux.ch>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,14 @@ whoami
 cd /home/user
 pwd
 ls -l
-CORE_COUNT="$(grep -c ^processor /proc/cpuinfo)"
+
+if [[ -z $2 ]]; then
+    CORE_COUNT="$(grep -c ^processor /proc/cpuinfo)"
+else
+    CORE_COUNT="$2"
+fi
+
+echo "Jobs: ${CORE_COUNT}"
 
 case "$1" in
     "linux-4.14.43"|linux)
@@ -18,11 +25,11 @@ case "$1" in
     "qt-everywhere-opensource-src-5.7.1"|qt)
         time tar xf qt-everywhere-opensource-src-5.7.1.tar.gz
         cd qt-everywhere-opensource-src-5.7.1
-        time ./configure -opensource -confirm-license
+        time ./configure -opensource -confirm-license -no-compile-examples
         time make -j ${CORE_COUNT}
         ;;
     *)
-        echo "Please run: docker run --user user --rm -i build-benchmark [linux-4.14.43|qt-everywhere-opensource-src-5.7.1]"
+        echo "Please run: docker run --user user --rm -i build-benchmark [linux-4.14.43|qt-everywhere-opensource-src-5.7.1] [n jobs]"
         ;;
 esac
 
